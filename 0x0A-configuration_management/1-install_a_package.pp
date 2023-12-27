3ad47182b860e8f6511c46f08b617f7f79575fbc
Chris Halogen <christackoms@gmail.com>
2023-12-28 00:13:20 +0100

    Fix Install Flask

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,5 +1,11 @@
 # Install Flask from pip3
-package { 'flask':
-  ensure   => '2.1.0',
-  provider => 'pip3',
+# Ensure that pip3 is present
+package { 'python3-pip':
+  ensure => present,
+}
+# Use pip3 to install flask
+exec { 'install_flask':
+  command => '/usr/bin/pip3 install Flask==2.1.0',
+  path    => '/usr/bin',
+  require => Package['python3-pip'],
 }
