4a947e038a5f75c5259f59ebb67f28338de0a7d3
Chris Halogen <christackoms@gmail.com>
2023-12-27 23:57:00 +0100

    First Push

---
+++ 0x0A-configuration_management/2-execute_a_command.pp
@@ -0,0 +1,5 @@
+# run bash command
+exec { 'kill':
+  command => 'pkill -f killmenow',
+  path    => ['/usr/bin', '/usr/sbin']
+}
---
+++ 0x0A-configuration_management/README.md
@@ -0,0 +1,3 @@
+# CM
+
+This repo consists of solution to Configuration Challenges at ALX
